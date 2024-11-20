788efc129ed661674bb29faa32f220f8b88c6280
H-Patt-ws <h.patterson-23@student.lboro.ac.uk>
2024-11-20 12:07:07 +0000

    created remote test 2

---
+++ Arduino Code/Comms to Robot/remote_test_2_robot/remote_test_2_robot.ino
@@ -0,0 +1,71 @@
+//Code for Slave module//
+#include <Servo.h>
+#include <CharonMovement.h>
+Servo RightWheel;
+Servo LeftWheel;
+Motion Move = Motion(RightWheel, LeftWheel);
+
+#define ledPin 8
+#define slaveSwitchPin 7
+int dataFromMaster = 0;
+
+void setup() {
+  Move.Attach(12, 13);
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
+  pinMode(slaveSwitchPin, INPUT);
+  digitalWrite(slaveSwitchPin,LOW);
+  Serial.begin(38400); // Default baud rate of the Bluetooth module
+}
+
+
+void loop() {
+ if(Serial.available() > 0){ // Checks whether data is comming from the serial port
+   dataFromMaster = 9;
+   dataFromMaster = Serial.read(); // Reads the data from the serial port and store it in dataFromMaster variable
+   Serial.println(dataFromMaster);
+ }
+
+switch (dataFromMaster){
+  case ('0'):{
+    Move.StayStill();
+    Serial.println("Stop!");
+  }
+  break;
+  case ('1'):{
+    Move.Forward();
+    Serial.println("Forth!");
+  }
+  break;
+  case ('3'):{
+    Move.RightTurn();
+    Serial.println("Turn right!");
+  }
+  break;
+  case ('5'):{
+    Move.Backward();
+    Serial.println("Back!");
+  }
+  break;
+  case ('7'):{
+    Move.LeftTurn();
+    Serial.println("Turn left!");
+  }
+  break;
+  default:{
+    Move.StayStill();
+    Serial.println("No Signal!");
+  }
+}
+ 
+ // Reading the slaveSwitchValue
+ //int slaveSwitchValue = digitalRead(slaveSwitchPin);
+ //if (slaveSwitchValue == HIGH) {
+ //  Serial.write('1'); // Sends '1' to the master to turn on BUZZER
+ //}
+ //else {
+ //  Serial.write('0');
+ //}  
+
+ delay(10);
+}
