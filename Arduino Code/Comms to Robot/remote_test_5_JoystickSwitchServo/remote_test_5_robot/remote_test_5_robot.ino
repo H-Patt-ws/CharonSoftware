47e9ac37df48c672a25ec130848678e8253c7025
PranayElligaram <106969368+PranayElligaram@users.noreply.github.com>
2025-04-07 22:44:21 +0100

    Update remote_test_5_robot.ino

--- Arduino Code/Comms to Robot/remote_test_5_JoystickSwitchServo/remote_test_5_robot/remote_test_5_robot.ino
+++ Arduino Code/Comms to Robot/remote_test_5_JoystickSwitchServo/remote_test_5_robot/remote_test_5_robot.ino
@@ -1,4 +1,4 @@
-//Code for Slave module//
+//Code for Robot(Slave module)//
 
 
 #include <Servo.h>
@@ -12,21 +12,37 @@ Servo LeftWheel;
 Motion Move = Motion(RightWheel, LeftWheel);
 
 #define buzzer 8
-
 #define slaveSwitchPin 7
+#define SERVO_PIN 10
+
+// Define directions the servo turns for each step of the token system
+#define PICKUP 0    // direction for picking up the token (clockwise)
+#define CARRY 90    // stop motor when carrying the token
+#define DROP 180    // direction for dropping the token (anticlockwise)
+
+Servo myServo;
+
 int dataFromMaster = 0;
 int movMode;
 int orderSwitch = 0; //order from the switch this cycle. "Carry" is denoted as 0, "Pick up" as 1 and "Drop" as 2
 int lastSwitch = 0; //order from the switch last cycle
 
+int signal = 0;
+int lastSignal = 0;
+
 void setup() {
-  Move.Attach(12, 13);
+  Move.Attach(13, 12);
   pinMode(buzzer, OUTPUT); 
   digitalWrite(buzzer, LOW);
   pinMode(slaveSwitchPin, INPUT);
   digitalWrite(slaveSwitchPin,LOW);
+  myServo.attach(SERVO_PIN);
+  pinMode(11,INPUT);
+
   Serial.begin(9600); // Default baud rate of the Bluetooth module
   BTSerial.begin(38400);
+
+  myServo.write(CARRY); // Starts the servo in the carry position
 }
 
 void loop() {
@@ -40,15 +56,19 @@ void loop() {
  if (dataFromMaster>=0 && dataFromMaster<=8){
   //Serial.println("toggle on");
   orderSwitch = 0; //"Carry" order
+  if (lastSwitch != orderSwitch){         //if switch reads a different signal to the last, it runs the token system code
+    moveToken(lastSwitch, orderSwitch);
+  }
   movMode = dataFromMaster;
   Serial.println(movMode);
   movement(movMode);
  }
  else if (dataFromMaster>= 16 && dataFromMaster<=24) {
   Serial.println("toggle half");
-  orderSwitch = 2; //"Drop" order
-  if (lastSwitch != orderSwitch){
+  orderSwitch = -1; //"Drop" order
+  if (lastSwitch != orderSwitch){       //if switch reads a different signal to the last, it runs the token system code
     tone(buzzer, 262, 500);
+    moveToken(lastSwitch, orderSwitch);
   }
   movMode = dataFromMaster - 16;
   Serial.println(movMode);
@@ -57,8 +77,9 @@ void loop() {
  else if (dataFromMaster>= 32 && dataFromMaster<=40){
   Serial.println("toggle half");
   orderSwitch = 1; //"Pick up" order
-  if (lastSwitch != orderSwitch){
+  if (lastSwitch != orderSwitch){           //if switch reads a different signal to the last, it runs the token system code
     tone(buzzer, 392, 500);
+    moveToken(lastSwitch, orderSwitch);
   }
   movMode = dataFromMaster - 32;
   Serial.println(movMode);
@@ -71,6 +92,22 @@ void loop() {
   //Serial.println("an error has occured");
  }
 lastSwitch = orderSwitch;
+
+ 
+  if(digitalRead(11)==LOW)  {
+    signal = 1;
+  }
+  else  {
+      signal = 0;
+  }
+  if (signal == 1 && lastSignal == 0){
+    Serial.println("Token");
+  }
+  else if (signal == 0 && lastSignal == 1){
+    Serial.println("Nothing");
+  }
+lastSignal = signal;
+
 }
 
 void movement(int movMode){
@@ -134,5 +171,22 @@ void movement(int movMode){
       //Serial.println("9");
     }
   }
-  delay(10);
+}
+
+
+  // Function to pick up the token 
+void moveToken(int lastSwitch,int orderSwitch) {
+
+  int diff = orderSwitch - lastSwitch; 
+  if (diff > 0){              //if the difference is positive the screw has to be moved downwards (drop->carry, carry->pickup, drop->pickup)
+    myServo.write (PICKUP);
+    delay(1500*diff);         //if the system should be moved from drop to pickup the screw should turn for double the amount of time than when moving between adjacent positions
+    myServo.write(90); 
+  }
+  else{                       //if the difference is negative the screw has to be moved upwards (carry->drop, pickup->carry, pickup->drop)
+    myServo.write(DROP);      
+    delay (1500*abs(diff));   //if the system should be moved from pickup to drop the screw should turn for double the amount of time than when moving between adjacent positions
+    myServo.write(90); 
+  }
+
 }
